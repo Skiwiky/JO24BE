74d4cbe35aaf9bc5a703105fc91f93ae2454da72
Sapioce <bertrand.bacon.36@gmail.com>
2024-03-07 21:20:07 +0100

    Creation et premier depot du git

---
+++ src/main/java/com/studi/sapioce/JO24BE/Jo24BeApplication.java
@@ -0,0 +1,13 @@
+package com.studi.sapioce.JO24BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jo24BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jo24BeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studi/sapioce/JO24BE/Jo24BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studi.sapioce.JO24BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jo24BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
