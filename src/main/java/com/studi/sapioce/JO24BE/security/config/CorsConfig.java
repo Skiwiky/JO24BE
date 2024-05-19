2e7626ea7b3c99ada6c86da0a32c5468d947bb2f
Sapioce <bertrand.bacon.36@gmail.com>
2024-05-19 15:48:58 +0200

    modification CORS

--- src/main/java/com/studi/sapioce/JO24BE/security/config/CorsConfig.java
+++ src/main/java/com/studi/sapioce/JO24BE/security/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		CorsConfiguration config = new CorsConfiguration();
 		config.setAllowCredentials(true);
-		config.addAllowedOrigin("https://jo2024sapioce.web.app");
+		config.addAllowedOrigin("https://jo2024sapioce.web.app/");
 		config.addAllowedHeader("*");
 		config.addAllowedMethod("*");
 		source.registerCorsConfiguration("/**", config);
